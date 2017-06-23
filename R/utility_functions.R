8627fbba8743a6047808c414375cf34181c560eb
Are Strom <are.strom@gmail.com>
2017-06-23 08:17:37 -0700

    Updated format of roxgen documentation

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -12,7 +12,7 @@ n_omit = function(x) c(n=length(na.omit(x)))
 #' Trim whitespace before and after string
 #' @rdname trim
 #' @param x a character vector
-#' @return Vector \code{x} of strings with whitespace on either side removed
+#' @return A vector \code{x} of strings with whitespace removed
 #' @examples
 #' fish_code = c("STHD ", " CHUM", " PINK ", "COHO")
 #' trim(fish_code)
@@ -23,7 +23,8 @@ trim = function(x) gsub("^[[:space:]]+|[[:space:]]+$", "", x)
 #' @rdname set_na
 #' @param x a character vector
 #' @param na_value a value such as "" or "9999" that should be converted to NA
-#' @examples # Convert empty string values "" to NA
+#' @return A vector \code{x} of strings with indicators of missing values converted to NA
+#' @examples
 #' fish_missing = c("STHD", "", "CHUM", "CHIN")
 #' fish_na = set_na(fish_missing)
 #' @export
@@ -35,7 +36,8 @@ set_na = function(x, na_value = "") {
 #' Convert NAs to empty strings ("")
 #' @rdname set_empty
 #' @param x a character vector
-#' @examples # Convert NA string values to empty ("") string.
+#' @return A vector \code{x} of strings with NAs converted to an empty string ("")
+#' @examples
 #' fish_na = c("STHD", NA, "CHUM", "CHIN")
 #' fish_missing = set_empty(fish_na)
 #' @export
@@ -47,7 +49,8 @@ set_empty = function(x) {
 #' Trim trailing semi-colon from a vector of strings
 #' @rdname trim_semi_colon
 #' @param x a character vector
-#' @examples # Get rid of a trailing semi-colon
+#' @return A vector \code{x} of strings with trailing semi-colons stripped out
+#' @examples
 #' observers = c("Austin;Wesley;", "Losee;Madel", "Morris;Starling;")
 #' observers_fixed = trim_semi_colon(observers)
 #' @export
@@ -57,7 +60,10 @@ trim_semi_colon = function(x) sub("[;]+$", "", x)
 #' @rdname f2c
 #' @param x an integer or numeric vector
 #' @param dec an integer specifying the number of decimals of rounding
-#' @examples # Convert from fahrenheit to celsius
+#' @return A vector \code{x} of temperature measurements converted from
+#'   fahrenheit to celsius. The number of decimals returned is set using
+#'   `dec`.
+#' @examples
 #' temp_f = c(32, 70.356, 212)
 #' temp_c = f2c(temp_f, dec = 2)
 #' @export
@@ -67,7 +73,10 @@ f2c = function(x, dec = 1) round((5/9) * (x - 32), dec)
 #' @rdname c2f
 #' @param x an integer or numeric vector
 #' @param dec an integer specifying the number of decimals of rounding
-#' @examples # Convert from celsius to fahrenheit
+#' @return A vector \code{x} of temperature measurements converted from
+#'   celsius to fahrenheit. The number of decimals returned is set using
+#'   `dec`.
+#' @examples
 #' temp_c = c(0.00, 21.31, 100.00)
 #' temp_f = c2f(temp_c, dec = 1)
 #' @export
