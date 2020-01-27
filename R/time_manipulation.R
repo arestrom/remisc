1a24d7f08a816908a10024e5988300fe1ec7487f
Are Strom <are.strom@gmail.com>
2020-01-27 13:55:35 -0800

    Documentation tweak

---
+++ R/time_manipulation.R
@@ -0,0 +1,20 @@
+mil_to_hm = function(x) {
+  require(dplyr)
+  mil = case_when(
+    x < 1 ~ '0000',
+    x > 0 & x < 10 ~ paste0('000', x),
+    x > 9 & x < 100 ~ paste0('00', x),
+    x > 99 & x < 1000 ~ paste0('0', x))
+  hrs = substr(mil, 1, 2)
+  mins = substr(mil, 3, 4)
+  tms = paste0(hrs, ":", mins)
+  return(tms)
+}
+
+# Function to convert time in minutes from midnight to h:m
+min_to_hm = function(x) {
+  h = floor(x / 60)
+  m = x %% 60
+  hm = paste0(h, ":", m)
+  return(hm)
+}
